b975ac764a293c12b8a8ab2a79b2965de0e8b5d8
logambika <145957150+imlogambika@users.noreply.github.com>
2025-05-23 01:00:17 +0530

    Update Serper.py

--- Serper.py
+++ Serper.py
@@ -3,7 +3,7 @@ import requests
 def search_query(query):
     url = "https://google.serper.dev/search"
     headers = {
-        "X-API-KEY": "5f385e13f38ac6f79f218084b7fca8446f08d5a0",  # Replace with your API Key
+        "X-API-KEY": "5f385e13f38ac6f79f218084b7fca8446f08d5a0",  #  API Key
         "Content-Type": "application/json"
     }
     payload = {
